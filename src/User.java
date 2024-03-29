2636d88b591910211741e4a0d68c2b5b4d8e34e3
SKrauter05 <stevenkrauter@yahoo.com>
2024-03-29 12:44:10 -0400

    //Added UserClass (3/29/24, 12:43 PM)

---
+++ src/User.java
@@ -0,0 +1,162 @@
+import java.util.ArrayList;
+import javax.imageio.ImageIO; //import for photos
+
+/**
+ * Group Project - User Class
+ * <p>
+ * This contains many methods & variables for
+ * creating a new User
+ *
+ * @author Steven Krauter, Lab 012
+ * @version Mar 28, 2024
+ */
+public class User {
+
+    public String username;
+    public String password;
+    public int age;
+    public ArrayList<String> friends;
+    public ArrayList<String> blockedUsers;
+    public boolean bFriendsOnly;
+    //profile pic
+
+    public User(String username, String password, int age) {
+
+        if (username == null) {
+            throw new NullPointerException();
+        }
+
+        if (!(age >= 18)) { //maybe change to <18?
+            throw new IllegalArgumentException();
+        }
+
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        friends = new ArrayList<>();
+        blockedUsers = new ArrayList<>();
+        bFriendsOnly = false;
+
+    } //end constructor
+
+    public String getUsername() { //return username
+        return username;
+    }
+
+    public void setUsername(String username) { //set username
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    } //return password
+
+    public void setPassword(String password) { //set password
+        this.password = password;
+    }
+
+    public int getAge() { //returns age
+        return age;
+    } //return age
+
+    public void setAge(int age) {
+
+        if (age >= 18) {
+            this.age = age;
+        } else {
+            System.out.println("Error: Age Must be at least 18");
+        }
+    } //sets age
+
+    public ArrayList<String> getFriends() { //returns friends ArrayList
+        return friends;
+    }
+
+    public void addFriends(String username) { //adds a friend, checks if already added, removes from blocks if there
+        if (friends.contains(username)) {
+            System.out.println("Friend already added!");
+        } else if (blockedUsers.contains(username)) {
+            removeBlockedUsers(username);
+            friends.add(username);
+        } else {
+            friends.add(username);
+        }
+    }
+
+    public void removeFriends(String username) { //removes a friend, checks if already in list
+        if (friends.contains(username)) {
+            friends.remove(username);
+        } else {
+            System.out.println("User not in friends list!");
+        }
+    }
+
+    public ArrayList<String> getBlockedUsers() { //returns blockedUser ArrayList
+        return blockedUsers;
+    }
+
+    public void addBlockedUsers(String username) { //adds blocked user, removes from friends if needed
+        if (blockedUsers.contains(username)) {
+            System.out.println("Already added to blocked users!");
+        } else {
+            if (friends.contains(username)) {
+                removeFriends(username);
+                blockedUsers.add(username);
+            } else {
+                blockedUsers.add(username);
+            }
+
+        }
+    }
+
+    public void removeBlockedUsers(String username) { //removes user from blocked
+        if (blockedUsers.contains(username)) {
+            blockedUsers.remove(username);
+        } else {
+            System.out.println("User not in blocked list!");
+        }
+    }
+
+    public boolean isbFriendsOnly() { //returns if friendsOnly T/F
+        return bFriendsOnly;
+    }
+
+    public void setbFriendsOnly(boolean bFriendsOnly) { //sets bFriendsOnly
+        this.bFriendsOnly = bFriendsOnly;
+    }
+
+    public String toString() { //returns a toString format
+
+        String output = "";
+
+        output += "Username: " + username + "\n";
+        output += "Password: " + password + "\n";
+        output += "Age: " + age + "\n";
+
+        if (getFriends().isEmpty()) {
+            output += "Friends: None" + "\n";
+
+        } else {
+            output += "Friends: " + friends + "\n";
+        }
+
+        if (getBlockedUsers().isEmpty()) {
+            output += "BlockedUsers: None" + "\n";
+        } else {
+            output += "BlockedUsers: " + blockedUsers + "\n";
+        }
+
+        return output;
+    }
+
+    /*
+    toString format:
+
+    Username: David Matthews
+    Password: password123
+    Age: 21
+    Friends: [Mark, John Matthews, NameHere]
+    BlockedUsers: None //if 'none' condition is true
+     */
+
+} //end class
