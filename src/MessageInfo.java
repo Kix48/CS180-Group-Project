f09f7a3e3131c76d460733a298acde48e5fb6d5b
jjmine6 <164970017+jjmine6@users.noreply.github.com>
2024-04-01 00:12:31 -0400

    commented methods

--- src/MessageInfo.java
+++ src/MessageInfo.java
@@ -4,27 +4,27 @@ public class MessageInfo implements MessageInfoInterface {
     private String receiver;
     private Date date;
 
-    public MessageInfo(String sender, String receiver){
+    public MessageInfo(String sender, String receiver){ //general constructor to create a new message (text or picture)
         this.sender = sender;
         this.receiver = receiver;
         this.date = new Date();
     }
 
-    public MessageInfo(String sender, String receiver, Date date){
+    public MessageInfo(String sender, String receiver, Date date){ //general constructor to create a new message with a known date(text or picture)
         this.sender = sender;
         this.receiver = receiver;
         this.date = date;
     }
 
-    public String getSender() {
+    public String getSender() { //returns the sender of the message
         return sender;
     }
 
-    public String getReceiver() {
+    public String getReceiver() { //returns the receiver of the message
         return receiver;
     }
 
-    public Date getDate() {
+    public Date getDate() { //returns the date of the mmessage
         return date;
     }
 }
