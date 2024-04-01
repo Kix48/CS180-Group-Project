769885dc284b79c09fcaeb8d8d645ef4337d5c2d
jjmine6 <164970017+jjmine6@users.noreply.github.com>
2024-03-31 23:50:56 -0400

    Update Message.java
    added comments

--- src/Message.java
+++ src/Message.java
@@ -2,25 +2,25 @@ import java.util.Date;
 
 public class Message extends MessageInfo implements MessageInterface {
     String message;
-    public Message(String sender, String receiver, String message) {
+    public Message(String sender, String receiver, String message) { //utilizes super class and creates a new message with the current time
         super(sender, receiver);
         this.message = message;
     }
 
-    public Message(String sender, String receiver, String message, Date date) {
+    public Message(String sender, String receiver, String message, Date date) {  //utilizes super class and creates a new message with a custom time
         super(sender, receiver, date);
         this.message = message;
     }
 
-    public String getMessage() {
+    public String getMessage() { //retrieve message text
         return this.message;
     }
 
-    public void setMessage(String message) {
+    public void setMessage(String message) { //edit and change the message text
         this.message = message;
     }
 
-    public String toString(){
+    public String toString(){ //print the message in a chat legible format
         return "[" + super.getDate().toString() + "] " +
                 super.getSender() + "-" + this.getReceiver() + ": " + message;
     }
